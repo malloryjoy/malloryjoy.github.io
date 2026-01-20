65456ca12e854abea9c915f0bfb50930d5f41d0f
Mallory Joy <malloryjoy13@gmail.com>
2026-01-19 22:58:44 -0500

    Changed outline to make image options possible

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ class About extends React.Component {
             <div className="my-10 -mx-5 rounded-xl py-5 px-5 border-t-6 
             border-r-6 border-l-12 border-b-12 bg-bio-purple border-medium-purple dark:bg-dark-header
              dark:border-dark-border w-[32em] h-[35em] md:min-w-[30em] md:-mx-[5em]">
-            <h2 className="flex initial font-header text-left  text-3xl my-5 text-medium-purple dark:text-skills-pink" aria-label="Hi! I'm Mallory.">Hi! I'm Mallory. <img src="/assets/bubble1.gif" className="block h-[2em] w-[7em] ml-[.5em] -mt-[1em] dark:hidden"></img><img src="/assets/sparkle1.gif" className="hidden h-[3em] w-[7.5em] ml-[.25em] -mt-[1em] dark:block"></img></h2>
+            <h2 className="flex initial font-header text-left  text-3xl my-5 text-medium-purple dark:text-skills-pink" aria-label="Hi! I'm Mallory.">Hi! I'm Mallory. <img src="/assets/bubble1.gif" className="block h-[2em] w-[7em] ml-[.5em] -mt-[1em] dark:hidden"></img><img src="assets/sparkle1.gif" className="hidden h-[3em] w-[7.5em] ml-[.25em] -mt-[1em] dark:block"></img></h2>
             <p className="flex initial font-body font-bold text-left text-medium-purple dark:text-skills-pink dark:-mt-5">I am a front-end developer based out of the east coast of the United States. 
                 The beginning of my career can be traced back to my early teens, as every ‘cool kid’ started creating 
                 profiles on myspace. I would rapid fire choose theme after theme for my own profile, while sitting in awe 
