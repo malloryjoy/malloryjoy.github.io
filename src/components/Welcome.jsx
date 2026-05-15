6e87817c026e366369235bcd0030aaaf69bcf885
Mallory Joy <malloryjoy13@gmail.com>
2026-05-15 00:15:58 -0400

    Replace header images and modify "About" section to have resized gifs

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,9 +4,10 @@ import Toggle from './Toggle';
 import '/src/index.css';
 class Welcome extends React.Component {
 
-    render() {
-        
-        return (<><section id="welcome" role="region" aria-label="my portfolio" className="bg-light-purple dark:bg-dark-body" >
+    
+
+    render() {        
+        return (<><section id="welcome" role="region" aria-label="my portfolio" className="bg-[url(public/assets/lightbg2.png)] bg-cover dark:bg-[url(public/assets/dark_bg1.png)]" >
             <Toggle />
             <Nav />
             <div id="hd-lspcmn" className="pt-20"></div>
@@ -14,9 +15,9 @@ class Welcome extends React.Component {
            <h1 className="font-header text-6xl text-dark-purple dark:text-skills-pink">MALLORY JOY</h1>
            <div className="py-10 font-header text-xl text-dark-purple dark:text-skills-pink"><p>Creating Magic, One Design at a Time</p></div>
            <div className="mt-5 pb-20 text-4xl text-medium-purple dark:text-skills-pink">
+            <a href="https://www.github.com/malloryjoy"><i class="fa-brands fa-github"></i></a>
+            <a className="mx-4" href="https://www.linkedin.com/in/malloryjoy/"><i class="fa-brands fa-linkedin-in"></i></a>
             <a href="https://www.instagram.com/malloryjoycreates"><i class="fa-brands fa-instagram"></i></a>
-            <a className="mx-4" href="https://www.github.com/malloryjoy"><i class="fa-brands fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/malloryjoy/"><i class="fa-brands fa-linkedin-in"></i></a>
             </div>
             <div className="pb-20 font-extrabold text-3xl text-dark-purple dark:text-skills-pink"><p><a aria-label="Scroll down to about me section" href="#aboutme"><i class="fa-solid fa-chevron-down"></i></a></p></div>
        </div>
